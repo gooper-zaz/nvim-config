07aba9083ec7adca81c73a3ed10ebff0b3c93c47
gan jianxiong <jianxiong.gan@zixel.cn>
2023-11-26 18:51:34 +0800

    chore: user LazyVim

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { { "prettierd", "eslint_d" } },
+        javascriptreact = { { "prettierd", "eslint_d" } },
+        typescript = { { "prettierd", "eslint_d" } },
+        typescriptreact = { { "prettierd", "eslint_d" } },
+        vue = { { "prettierd", "eslint_d" } },
+        json = { { "prettierd", "prettier" } },
+        jsonc = { { "prettierd", "prettier" } },
+        css = { { "prettierd", "prettier" } },
+        html = { { "prettierd", "prettier" } },
+        yaml = { { "prettierd", "prettier" } },
+        sass = { { "prettierd", "prettier" } },
+        less = { { "prettierd", "prettier" } },
+        markdown = { { "prettierd", "prettier" } },
+      },
+    },
+  },
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,258 +1,206 @@
--- require("lspconfig").lua_ls.setup {
---   capabilities = capabilities,
---   group = vim.api.nvim_create_augroup('LspFormatting', { clear = true }),
---   on_attach = function(_, bufnr)
---     on_attach(_, bufnr)
---   end
--- }
-local utils = require("utils")
-local consts = require("contants")
-
 return {
-	"neovim/nvim-lspconfig",
-	cmd = { "Mason", "Neoconf" },
-	event = { "BufReadPost", "BufNewFile" },
-	dependencies = {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim", -- 这个相当于mason.nvim和lspconfig的桥梁
-		"onsails/lspkind-nvim",
-		"folke/neoconf.nvim",
-		"folke/neodev.nvim",
-		{
-			"j-hui/fidget.nvim",
-			tag = "legacy",
-		},
-		"nvimdev/lspsaga.nvim",
-		"SmiteshP/nvim-navic",
-		-- "jose-elias-alvarez/typescript.nvim",
-		"b0o/schemastore.nvim",
-	},
-	config = function()
-		local on_attach = function(client, bufnr)
-			-- 定义快捷键的函数
-			-- @param keys string
-			local nmap = function(keys, func, desc)
-				if desc then
-					desc = "LSP: " .. desc
-				end
-				vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-			end
-
-			-- 定义快捷键
-			-- 跳转到声明
-			nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-			-- 跳转到定义
-			nmap("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-			-- hover显示提示信息
-			nmap("<leader>k", "<cmd>Lspsaga hover_doc<CR>", "Hover Documentation")
-			-- 跳转到实现
-			nmap("gi", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-			-- 签名信息
-			nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-			nmap("<C-f>", "<cmd>Lspsaga finder<CR>", "Lspsaga [F]inder")
-			-- 工作区添加文件夹
-			nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-			-- 工作区删除文件夹
-			nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-			-- 工作区文件列表
-			nmap("<leader>wl", function()
-				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-			end, "[W]orkspace [L]ist Folders")
-			nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-			-- 重命名
-			nmap("<leader>rn", "<cmd>Lspsaga rename ++project<CR>", "[R]e[n]ame")
-			-- code action
-			nmap("<leader>ca", "<cmd>Lspsaga code_action<CR>", "[C]ode [A]ction")
-			-- 显示代码诊断
-			nmap("<leader>da", require("telescope.builtin").diagnostics, "[D]i[A]gnostics")
-			-- 显示引用信息
-			nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-
-			-- 格式化代码
-			nmap("<space>f", function()
-				vim.lsp.buf.format({ async = true })
-			end, "[F]ormat code")
-
-			-- format on save
-			-- vim.api.nvim_create_autocmd("BufWritePre", {
-			--   buffer = bufnr,
-			--   callback = function()
-			--     vim.lsp.buf.format()
-			--   end,
-			-- })
-			if client.server_capabilities.documentSymbolProvider then
-				local navic = require("nvim-navic")
-				navic.attach(client, bufnr)
-			end
-		end
-
-		local servers = {
-			lua_ls = {
-				settings = {
-					Lua = {
-						workspace = { checkThirdParty = false },
-						telemetry = { enable = false },
-						diagnostics = {
-							globals = { "vim" },
-						},
-					},
-				},
-			},
-			clangd = {},
-			-- tsserver = { disabled = true },
-			-- 使用volar代替tsserver, 以便在vue文件中也能使用lsp
-			volar = {
-				settings = {
-					volar = {
-						codeLens = {
-							references = true,
-							pugTools = true,
-							scriptSetupTools = true,
-						},
-					},
-				},
-				filetypes = {
-					"javascript",
-					"javascriptreact",
-					"typescript",
-					"typescriptreact",
-					"javascript.jsx",
-					"typescript.tsx",
-					"vue",
-					-- "json",
-				},
-				init_options = {
-					documentFeatures = {
-						documentColor = false,
-						documentFormatting = {
-							defaultPrintWidth = 100,
-						},
-						documentSymbol = true,
-						foldingRange = true,
-						linkedEditingRange = true,
-						selectionRange = true,
-					},
-					languageFeatures = {
-						callHierarchy = true,
-						codeAction = true,
-						codeLens = true,
-						completion = {
-							defaultAttrNameCase = "kebabCase",
-							defaultTagNameCase = "both",
-						},
-						definition = true,
-						diagnostics = true,
-						documentHighlight = true,
-						documentLink = true,
-						hover = true,
-						implementation = true,
-						references = true,
-						rename = true,
-						renameFileRefactoring = true,
-						schemaRequestService = true,
-						semanticTokens = false,
-						signatureHelp = true,
-						typeDefinition = true,
-					},
-					typescript = {
-						serverPath = consts.global_ts_server,
-						-- 使用全局的typescript而不是node_module中的typescript, 你可以执行`npm install -g typescript`来安装
-						tsdk = consts.global_ts_server_lib,
-					},
-				},
-				on_new_config = function(new_config, new_root_dir)
-					new_config.init_options.typescript.serverPath = utils.get_typescript_server_path(new_root_dir)
-					-- new_config.init_options.typescript.tsdk = utils.get_typescript_server_path(new_root_dir)
-				end,
-			},
-			bashls = {},
-			jsonls = {
-				settings = {
-					json = {
-						schemas = require("schemastore").json.schemas(),
-						validate = { enable = true },
-					},
-				},
-			},
-		}
-
-		require("neoconf").setup()
-		require("neodev").setup()
-		require("fidget").setup()
-
-		require("lspsaga").setup({
-			ui = {
-				border = "rounded",
-			},
-		})
-
-		require("mason").setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
-
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-		require("mason-lspconfig").setup({
-			-- 确保安装，根据需要填写
-			ensure_installed = vim.tbl_keys(servers),
-			automatic_installation = true,
-			-- handlers = {
-			--   function(server_name) -- default handler (optional)
-			--     require("lspconfig")[server_name].setup({
-			--       settings = servers[server_name],
-			--       on_attach = on_attach,
-			--       capabilities = capabilities,
-			--     })
-			--   end,
-			-- },
-		})
-		for server, config in pairs(servers) do
-			require("lspconfig")[server].setup(vim.tbl_deep_extend("keep", {
-				on_attach = on_attach,
-				capabilities = capabilities,
-			}, config))
-		end
-
-		require("lspkind").init({
-			mode = "symbol_text",
-			preset = "codicons",
-			symbol_map = {
-				Text = "",
-				Method = "",
-				Function = "",
-				Constructor = "",
-				Field = "ﰠ",
-				Variable = "",
-				Class = "ﴯ",
-				Interface = "",
-				Module = "",
-				Property = "ﰠ",
-				Unit = "塞",
-				Value = "",
-				Enum = "",
-				Keyword = "",
-				Snippet = "",
-				Color = "",
-				File = "",
-				Reference = "",
-				Folder = "",
-				EnumMember = "",
-				Constant = "",
-				Struct = "פּ",
-				Event = "",
-				Operator = "",
-				TypeParameter = "",
-			},
-		})
-
-		-- vim.keymap.set('n', '<leader>o', '<cmd>TypescriptOrganizeImports<cr>')
-		-- vim.keymap.set('n', '<leader>a', '<cmd>TypescriptAddMissingImports<cr>')
-		-- vim.keymap.set('n', '<leader>r', '<cmd>TypescriptRemoveUnused<cr>')
-	end,
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "stylua",
+        "css-lsp",
+        "vue-language-server",
+        "html-lsp",
+        -- "typescript-language-server",
+        "json-lsp",
+        "eslint-lsp",
+        "eslint_d",
+        "prettier",
+        "prettierd",
+        "emmet-ls",
+      })
+    end,
+  },
+  {
+    "b0o/schemastore.nvim",
+    -- dependencies = {
+    --   "folke/neodev.nvim",
+    -- },
+    opts = {},
+    ft = { "json", "jsonc" },
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      -- require("neodev").setup()
+      require("lspconfig").jsonls.setup({
+        settings = {
+          json = {
+            schemas = require("schemastore").json.schemas(),
+            validate = { enable = true },
+          },
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    cmd = { "Mason", "Neoconf" },
+    event = { "BufReadPost", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim", -- 这个相当于mason.nvim和lspconfig的桥梁
+      -- "jose-elias-alvarez/typescript.nvim",
+      "b0o/schemastore.nvim",
+    },
+    opts = {
+      inlay_hints = { enabled = true },
+      ---@type lspconfig.options
+      servers = {
+        lua_ls = {
+          single_file_support = true,
+          settings = {
+            Lua = {
+              workspace = { checkThirdParty = false },
+              telemetry = { enable = false },
+              diagnostics = {
+                globals = { "vim" },
+              },
+            },
+          },
+        },
+        cssls = {
+          settings = {},
+        },
+        emmet_ls = {
+          filetypes = {
+            "html",
+            "typescriptreact",
+            "javascriptreact",
+            "css",
+            "sass",
+            "scss",
+            "less",
+            "markdown",
+            "vue",
+          },
+          init_options = { -- better html/css snippets supported
+            html = {
+              options = {
+                -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L26
+                ["bem.enabled"] = true,
+              },
+            },
+          },
+        },
+        jsonls = {
+          -- settings = {
+          --   json = {
+          --     schemas = (function()
+          --       return require("schemastore").json.schemas()
+          --     end)(),
+          --     validate = { enable = true },
+          --   },
+          -- },
+        },
+        -- tsserver = {
+        --   root_dir = function(...)
+        --     local util = require("lspconfig.util")
+        --     local git_root = util.root_pattern(".git")(...)
+        --     if git_root then
+        --       return git_root
+        --     end
+        --     local lockfile_root = util.root_pattern("package-lock.json")(...)
+        --     if lockfile_root then
+        --       return lockfile_root
+        --     end
+        --     local pnpm_lock_root = util.root_pattern("pnpm-lock.yaml")(...)
+        --     if pnpm_lock_root then
+        --       return pnpm_lock_root
+        --     end
+        --     return require("lspconfig.util").root_pattern("node_modules")(...)
+        --   end,
+        --   single_file_support = false,
+        --   settings = {
+        --     typescript = {
+        --       inlayHints = {
+        --         includeInlayParameterNameHints = "literal",
+        --         includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        --         includeInlayFunctionParameterTypeHints = true,
+        --         includeInlayVariableTypeHints = false,
+        --         includeInlayPropertyDeclarationTypeHints = true,
+        --         includeInlayFunctionLikeReturnTypeHints = true,
+        --         includeInlayEnumMemberValueHints = true,
+        --       },
+        --     },
+        --     javascript = {
+        --       inlayHints = {
+        --         includeInlayParameterNameHints = "all",
+        --         includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        --         includeInlayFunctionParameterTypeHints = true,
+        --         includeInlayVariableTypeHints = true,
+        --         includeInlayPropertyDeclarationTypeHints = true,
+        --         includeInlayFunctionLikeReturnTypeHints = true,
+        --         includeInlayEnumMemberValueHints = true,
+        --       },
+        --     },
+        --   },
+        -- },
+        volar = {
+          root_dir = function(...)
+            local util = require("lspconfig.util")
+            -- local git_root = util.root_pattern(".git")(...)
+            -- if git_root then
+            --   return git_root
+            -- end
+            -- local lockfile_root = util.root_pattern("package-lock.json")(...)
+            -- if lockfile_root then
+            --   return lockfile_root
+            -- end
+            -- local pnpm_lock_root = util.root_pattern("pnpm-lock.yaml")(...)
+            -- if pnpm_lock_root then
+            --   return pnpm_lock_root
+            -- end
+            -- return require("lspconfig.util").root_pattern("node_modules")(...)
+            return util.root_pattern(".git")(...)
+          end,
+          filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+          settings = {
+            volar = {
+              codeLens = {
+                references = true,
+                pugTools = true,
+                scriptSetupTools = true,
+              },
+              takeOverMode = {
+                extension = "Vue.volar",
+              },
+            },
+            -- vue = {},
+            init_options = {
+              typescript = {
+                serverPath = "E:/nvm/node_global/node_modules/typescript/lib/tsserverlibrary.js",
+              },
+            },
+            -- on_new_config = function(config, root_dir)
+            --   local function get_ts_path(dir)
+            --     local util = require("lspconfig.util")
+            --
+            --     local global_ts = "E:/nvm/node_global/node_modules/typescript/lib/tsserverlibrary.js"
+            --     -- Alternative location if installed as root:
+            --     -- local global_ts = '/usr/local/lib/node_modules/typescript/lib/tsserverlibrary.js'
+            --     local found_ts = ""
+            --     local function check_dir(path)
+            --       found_ts = util.path.join(path, "node_modules", "typescript", "lib", "tsserverlibrary.js")
+            --       if util.path.exists(found_ts) then
+            --         return path
+            --       end
+            --     end
+            --     if util.search_ancestors(dir, check_dir) then
+            --       return found_ts
+            --     else
+            --       return global_ts
+            --     end
+            --   end
+            --   config.init_options.typescript.serverPath = get_ts_path(root_dir)
+            -- end,
+          },
+        },
+      },
+    },
+  },
 }
