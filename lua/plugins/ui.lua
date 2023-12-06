a6f9d93edce6df360b472d492791709915f7aad7
gan jianxiong <jianxiong.gan@zixel.cn>
2023-12-06 22:15:59 +0800

    feat: add colorizer plugin

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -22,4 +22,15 @@ return {
       opts.config.header = vim.split(logo, '\n')
     end,
   },
+  {
+    'NvChad/nvim-colorizer.lua',
+    event = { 'BufReadPost' },
+    config = function()
+      require('colorizer').setup({
+        user_default_options = {
+          mode = 'foreground',
+        },
+      })
+    end,
+  },
 }
