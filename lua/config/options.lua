f149b5a7cebfb4f90dd6de4841d4b678414d2a16
gooper-zaz <gooper.gan@foxmail.com>
2023-12-10 16:50:12 +0800

    feat: add fold seetings (#27)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- fold
+local opt = vim.opt
+opt.foldmethod = 'expr'
+opt.foldexpr = 'nvim_treesitter#foldexpr()'
+opt.foldenable = false
+
 -- neovide 配置
 if vim.g.neovide then
   -- 轨道动画
