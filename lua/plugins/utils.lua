1d7946c47b0c4a8abfa06cf463b2ebe52e1e44ec
gooper-zaz <gooper.gan@foxmail.com>
2023-12-13 22:39:11 +0800

    feat: gitsign settings && deps upgrade (#30)

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -6,4 +6,10 @@ return {
       require('better_escape').setup()
     end,
   },
+  {
+    'lewis6991/gitsigns.nvim',
+    keys = {
+      { '<leader>gb', mode = { 'n' }, '<Cmd>Gitsigns toggle_current_line_blame<CR>', desc = 'current line [G]it [B]lame' },
+    },
+  },
 }
