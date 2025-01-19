71a2d11a2855161e44921a2056684ba922130c29
gooper-zaz <gooper.gan@foxmail.com>
2025-01-19 20:10:11 +0800

    feat: scope highlight

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -14,7 +14,7 @@ return {
   },
   {
     'folke/snacks.nvim',
-    -- @type snacks.Config
+    --- @type snacks.Config
     opts = {
       dashboard = {
         preset = {
@@ -28,6 +28,26 @@ return {
           ╚═════╝  ╚═════╝  ╚═════╝ ╚═╝     ╚══════╝╚═╝  ╚═╝    ╚══════╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝
 
         ]],
+        },
+      },
+      scroll = { enabled = false },
+      indent = {
+        scope = {
+          enabled = true, -- enable highlighting the current scope
+          priority = 200,
+          char = '│',
+          underline = false, -- underline the start of the scope
+          only_current = true, -- only show scope in the current window
+          -- hl = "SnacksIndentScope", ---@type string|string[] hl group for scopes
+          hl = {
+            'RainbowRed',
+            'RainbowYellow',
+            'RainbowBlue',
+            'RainbowOrange',
+            'RainbowGreen',
+            'RainbowViolet',
+            'RainbowCyan',
+          },
         },
       },
     },
@@ -44,5 +64,57 @@ return {
         },
       })
     end,
+  },
+  {
+    'HiPhish/rainbow-delimiters.nvim',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
+    event = { 'BufReadPost' },
+    config = function()
+      local rainbow = require('rainbow-delimiters')
+      require('rainbow-delimiters.setup').setup({
+        strategy = {
+          [''] = rainbow.strategy['global'],
+          vim = rainbow.strategy['local'],
+        },
+        query = {
+          [''] = 'rainbow-delimiters',
+          lua = 'rainbow-blocks',
+        },
+        priority = {
+          [''] = 110,
+          lua = 210,
+        },
+        highlight = {
+          'RainbowDelimiterRed',
+          'RainbowDelimiterYellow',
+          'RainbowDelimiterBlue',
+          'RainbowDelimiterOrange',
+          'RainbowDelimiterGreen',
+          'RainbowDelimiterViolet',
+          'RainbowDelimiterCyan',
+        },
+      })
+    end,
+  },
+  {
+    'catppuccin/nvim',
+    lazy = true,
+    name = 'catppuccin',
+    opts = {
+      term_colors = true,
+      custom_highlights = function()
+        return {
+          RainbowRed = { fg = '#E06C75' },
+          RainbowYellow = { fg = '#E5C07B' },
+          RainbowBlue = { fg = '#61AFEF' },
+          RainbowOrange = { fg = '#D19A66' },
+          RainbowGreen = { fg = '#98C379' },
+          RainbowViolet = { fg = '#C678DD' },
+          RainbowCyan = { fg = '#56B6C2' },
+        }
+      end,
+    },
   },
 }
