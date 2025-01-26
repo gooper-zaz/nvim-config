3ce8f3ee09bcb1b960bc4bcd82a4dc3cc63235f4
gooper-zaz <gooper.gan@foxmail.com>
2025-01-26 22:38:57 +0800

    chore: upgrade deps

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -36,13 +36,23 @@ return {
         ['<C-p>'] = { 'snippet_forward', 'fallback' },
         ['<C-t>'] = { 'snippet_backward', 'fallback' },
 
-        cmdline = {
-          preset = 'enter',
-        },
+        -- cmdline = {
+        --   preset = 'enter',
+        -- },
       },
       completion = {
         menu = { border = 'single' },
-        documentation = { window = { border = 'single' } },
+        documentation = { auto_show = true, auto_show_delay_ms = 500, window = { border = 'single' } },
+        list = { selection = { preselect = false, auto_insert = true } },
+      },
+      sources = {
+        default = { 'buffer', 'lsp', 'path', 'snippets' },
+        providers = {
+          buffer = { score_offset = 1 },
+          path = { score_offset = 2 },
+          snippets = { score_offset = 3 },
+          lsp = { score_offset = 4 },
+        },
       },
       signature = { window = { border = 'single' } },
     },
