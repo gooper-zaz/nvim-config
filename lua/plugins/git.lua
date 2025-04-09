8678cf1ae58366167e6b8d607aea3007ae221b0c
gooper-zaz <gooper.gan@foxmail.com>
2025-04-09 19:35:09 +0800

    feat: improve keymaps of git operattions

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,7 +1,9 @@
+---@diagnostic disable: missing-fields
 return {
   {
     'lewis6991/gitsigns.nvim',
     event = 'VeryLazy',
+    ---@type Gitsigns.Config
     opts = {
       signs = {
         add = { text = '▎' },
@@ -18,6 +20,51 @@ return {
         topdelete = { text = '' },
         changedelete = { text = '▎' },
       },
+      current_line_blame = true,
+      current_line_blame_opts = {
+        delay = 500,
+      },
+      on_attach = function(buffer)
+        local gs = require('gitsigns')
+
+        local function map(mode, l, r, desc)
+          vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+        end
+
+        map('n', ']h', function()
+          if vim.wo.diff then
+            vim.cmd.normal({ ']c', bang = true })
+          else
+            gs.nav_hunk('next')
+          end
+        end, 'Next Hunk')
+
+        map('n', '[h', function()
+          if vim.wo.diff then
+            vim.cmd.normal({ '[c', bang = true })
+          else
+            gs.nav_hunk('prev')
+          end
+        end, 'Prev Hunk')
+
+        map('n', ']H', function()
+          gs.nav_hunk('last')
+        end, 'Last Hunk')
+
+        map('n', '[H', function()
+          gs.nav_hunk('first')
+        end, 'First Hunk')
+
+        map('n', '<leader>gp', gs.preview_hunk_inline, 'Preview Hunk Inline')
+
+        map('n', '<leader>gb', function()
+          gs.blame_line({ full = true })
+        end, 'Blame Line')
+
+        map('n', '<leader>gB', function()
+          gs.blame()
+        end, 'Blame Buffer')
+      end,
     },
     -- config = function(_, opts)
     --   require('gitsigns').setup(opts)
