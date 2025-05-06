cf6b6ab6341688b349473c4853f24bc2fc0f74c7
gooper-zaz <gooper.gan@foxmail.com>
2025-05-06 19:40:17 +0800

    feat: add todo-comments plugin (#62)
    feat: add todo-comments plugin to highlight some keywords like 'TODO'
    'FIXME' 'TEST' ...

--- lua/plugins/editors.lua
+++ lua/plugins/editors.lua
@@ -46,4 +46,14 @@ return {
     --   vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
     -- end
   },
+  {
+    'folke/todo-comments.nvim',
+    event = 'BufReadPost',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 }
