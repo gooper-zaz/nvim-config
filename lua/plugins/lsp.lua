9d5f1b4542092745e33b0084f3ec4bd913afb632
gooper-zaz <gooper.gan@foxmail.com>
2025-12-02 00:10:49 +0800

    feat(ui): update mason icons

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -303,6 +303,11 @@ return {
         border = 'rounded',
         width = 0.8,
         height = 0.8,
+        icons = {
+          package_installed = '✓',
+          package_pending = '➜',
+          package_uninstalled = '✗',
+        },
       },
     },
     config = function(_, opts)
