1f1824c361b803ffe4e48299a829dd8e3b7f26b4
gooper-zaz <gooper.gan@foxmail.com>
2026-05-12 23:14:14 +0800

    feat: add `opencode.nvim` plugin

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -15,8 +15,8 @@ return {
         auto_set_keymaps = false,
       },
       windows = {
-        width = 40
-      }
+        width = 40,
+      },
     },
     cmd = {
       'AvanteAsk',
@@ -46,5 +46,95 @@ return {
       { '<leader>as', '<cmd>AvanteStop<CR>', desc = 'Stop Avante' },
       { '<leader>at', '<cmd>AvanteToggle<CR>', desc = 'Toggle Avante' },
     },
+  },
+  -- opencode.nvim
+  {
+    'nickjvandyke/opencode.nvim',
+    -- version = '*', -- Latest stable release
+    dependencies = {
+      {
+        -- `snacks.nvim` integration is recommended, but optional
+        ---@module "snacks" <- Loads `snacks.nvim` types for configuration intellisense
+        'folke/snacks.nvim',
+        optional = true,
+        opts = {
+          input = {}, -- Enhances `ask()`
+          picker = { -- Enhances `select()`
+            actions = {
+              opencode_send = function(...)
+                return require('opencode').snacks_picker_send(...)
+              end,
+            },
+            win = {
+              input = {
+                keys = {
+                  ['<a-a>'] = { 'opencode_send', mode = { 'n', 'i' } },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    keys = {
+      {
+        '<leader>oa',
+        function()
+          require('opencode').ask('@buffer: ', { submit = true })
+        end,
+        mode = { 'n' },
+        desc = 'Opencode Ask (Buffer)',
+      },
+      {
+        '<leader>oa',
+        function()
+          require('opencode').ask('@this: ', { submit = true })
+        end,
+        mode = { 'v' },
+        desc = 'Opencode Ask (Selection)',
+      },
+      {
+        '<leader>ob',
+        function()
+          require('opencode').ask('@buffers: ', { submit = true })
+        end,
+        mode = { 'n' },
+        desc = 'Opencode Ask (All Buffers)',
+      },
+      {
+        '<leader>od',
+        function()
+          require('opencode').ask('@diagnostics: ', { submit = true })
+        end,
+        mode = { 'n' },
+        desc = 'Opencode Ask (Diagnostics)',
+      },
+    },
+    config = function()
+      ---@type opencode.Opts
+      vim.g.opencode_opts = {
+        -- server = {
+        --   start = function()
+        --     local ok = vim.fn.jobstart(
+        --       { 'wt.exe', '-w', '0', 'nt', '--title', 'opencode', 'powershell', '-NoExit', 'opencode', '--port' },
+        --       { detach = true }
+        --     )
+        --     if ok <= 0 then
+        --       vim.notify('wt.exe not found, starting opencode directly', vim.log.levels.WARN)
+        --       vim.fn.jobstart({ 'opencode', '--port' }, { detach = true })
+        --     end
+        --   end,
+        --   stop = function()
+        --     vim.fn.jobstart({ 'taskkill', '/f', '/im', 'opencode.exe' }, { detach = true })
+        --   end,
+        --   toggle = function()
+        --     vim.fn.jobstart(
+        --       { 'wt.exe', '-w', '0', 'nt', '--title', 'opencode', 'powershell', '-NoExit', 'opencode', '--port' },
+        --       { detach = true }
+        --     )
+        --   end,
+        -- },
+      }
+    end,
   },
 }
